539aea845567fccb01ede07b57af8a02ac7e7e04
Taimoor Tariq <taimoortariqdev@gmail.com>
2022-05-12 12:32:06 +0500

    Header navigation

--- components/Header.js
+++ components/Header.js
@@ -4,13 +4,14 @@ import { useState } from 'react';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/router';
 
 export default function Header() {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [noOfGuests, setNoOfGuests] = useState(1);
-
+    const router = useRouter();
     const resetInput = () => {
         setSearchInput('');
     }
@@ -29,7 +30,7 @@ export default function Header() {
     return (
         <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
             {/* left Logo*/}
-            <div className="relative flex items-center h-10 cursor-pointer my-auto">
+            <div onClick={ () => router.push('/')} className="relative flex items-center h-10 cursor-pointer my-auto">
                 <Image src="https://links.papareact.com/qd3"
                     layout="fill"
                     objectFit="contain"
